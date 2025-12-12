23e8dc8cc3c94da7d715ce8746239c5cb18b5399
gust553 <127223129+gust553@users.noreply.github.com>
2025-12-12 07:02:29 -0300

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,1 @@
+const express=require('express');const app=express();app.use(require('express').static('public'));const port=process.env.PORT||10000;app.listen(port,()=>console.log('Server on',port));
